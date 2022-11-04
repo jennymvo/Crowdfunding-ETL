f5cfa7b03067c39762e6a1fbd9e280b37983a2e3
jennymvo <jvo6@ucsc.edu>
2022-11-03 22:52:20 -0700

    Update challenge materials

--- crowdfunding_db_table_schema_example.sql
+++ crowdfunding_db_table_schema_example.sql
@@ -59,3 +59,17 @@ REFERENCES "category" ("category_id");
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
 REFERENCES "subcategory" ("subcategory_id");
 
+CREATE TABLE "backers" (
+    "backer_id" varchar   NOT NULL,
+    "cf_id" int   NOT NULL,
+    "first_name" varchar(50)   NOT NULL,
+    "last_name" varchar(50) NOT NULL,
+    "email" varchar(100)   NOT NULL,
+    CONSTRAINT "pk_backers" PRIMARY KEY (
+        "backer_id"
+     )
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_backers_id" FOREIGN KEY("backers_id")
+REFERENCES "backers" ("backers_id");
+
